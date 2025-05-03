a6eb1614412bb833208eeff01e2f908e3644cbf9
dishliysK9 <201221013@fdiba.tu-sofia.bg>
2025-05-03 23:00:04 +0300

    Add: React Routing for /home & /search, use the routing link when needed

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -4,13 +4,27 @@ import {Navbar} from "./layout/NavbarAndFooter/Navbar";
 import {Footer} from "./layout/NavbarAndFooter/Footer";
 import {HomePage} from "./layout/HomePage/HomePage";
 import {SearchBooksPage} from "./layout/SearchBookPage/SearchBooksPage";
+import {Redirect, Route, Switch} from "react-router-dom";
+
+//react is SPA (single page application)
 
 export const  App = () => {
   return (
-      <div>
+      <div className='d-flex flex-column min-vh-100'>
           <Navbar/>
-          {/*<HomePage/>*/}
-          <SearchBooksPage/>
+          <div className='flex-grow-1'>
+              <Switch>
+                  <Route path='/' exact> {/*Empty route and empty route only*/}
+                      <Redirect to='/home'/>
+                  </Route>
+                  <Route path='/home' >
+                      <HomePage/>
+                  </Route>
+                  <Route path='/search' >
+                      <SearchBooksPage/>
+                  </Route>
+              </Switch>
+          </div>
           <Footer/>
       </div>
   );
