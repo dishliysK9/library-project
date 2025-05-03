a1b5fcf0a21991dc6cb7c8e36748e71bf7bb55c7
dishliysK9 <201221013@fdiba.tu-sofia.bg>
2025-05-03 18:49:29 +0300

    Add: Pagination for SearchBooksPage component

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import {Navbar} from "./layout/NavbarAndFooter/Navbar";
 import {Footer} from "./layout/NavbarAndFooter/Footer";
 import {HomePage} from "./layout/HomePage/HomePage";
+import {SearchBooksPage} from "./layout/SearchBookPage/SearchBooksPage";
 
 export const  App = () => {
   return (
       <div>
           <Navbar/>
-          <HomePage/>
+          {/*<HomePage/>*/}
+          <SearchBooksPage/>
           <Footer/>
       </div>
   );
---
+++ frontend/react-library/src/layout/SearchBookPage/SearchBooksPage.tsx
@@ -0,0 +1,157 @@
+import {useEffect, useState} from "react";
+import {BookModel} from "../../models/BookModel";
+import {SpinnerLoading} from "../Utils/SpinnerLoading";
+import {SearchBook} from "./components/SearchBook";
+import {Pagination} from "../Utils/Pagination";
+
+export const SearchBooksPage = () => {
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [booksPerPage] = useState(5);
+    const [totalAmmountOfBooks, setTotalAmountOfBooks] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const baseUrl: string = 'http://localhost:8080/api/books';
+
+            // carousell has 3 swipes with 3 books each swipe, therefore we need 9 in total top books
+            const url: string =`${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+
+            // async fetch url data
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJson = await response.json();
+
+            //here we have all of the books
+            const responseData = responseJson._embedded.books;
+
+            setTotalAmountOfBooks(responseJson.page.totalElements);
+            setTotalPages(responseJson.page.totalPages);
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData){
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img,
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
+        };
+        fetchBooks().catch((error: any)=> {
+            setIsLoading(false);
+            setHttpError(error);
+        })
+        //every time this useEffect is called, we scroll to the top [real nice]
+        window.scrollTo(0, 0);
+        //each time a single piece of state changes that we type in here, recall this useEffect hook
+    }, [currentPage]);
+
+    if (isLoading) {
+        return (
+            <SpinnerLoading/>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    const indexOfLastBook: number = currentPage * booksPerPage;
+    const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+    let lastItem = booksPerPage *currentPage <= totalAmmountOfBooks ?
+        booksPerPage * currentPage : totalAmmountOfBooks;
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    return (
+        <div>
+            <div className='container'>
+                <div>
+                    <div className='row mt-5'>
+                        <div className='col-6'>
+                            <div className='d-flex'>
+                                <input className='form-control me-2' type='search'
+                                       placeholder='Search' aria-labelledby='Search'/>
+                                <button className='btn btn-outline-success'>
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+                        <div className='col-4'>
+                            <div className='dropdown'>
+                                <button className='btn btn-secondary dropdown-toggle' type='button'
+                                        id='dropdownMenuButton1' data-bs-toggle='dropdown'
+                                        aria-expanded='false'>
+                                    Category
+                                </button>
+                                <ul className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
+                                    <li>
+                                        <a className='dropdown-item' href='#'>
+                                            All
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='dropdown-item' href='#'>
+                                            Front End
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='dropdown-item' href='#'>
+                                            Back End
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='dropdown-item' href='#'>
+                                            Data
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className='dropdown-item' href='#'>
+                                            DevOps
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='mt-3'>
+                        <h5>Number of results: ({totalAmmountOfBooks})</h5>
+                    </div>
+                    <p>
+                        {indexOfFirstBook + 1} to {lastItem} of {totalAmmountOfBooks} items:
+                    </p>
+                    {books.map(book => (
+                        <SearchBook book={book} key={book.id} />
+                    ))}
+                    {totalPages > 1 &&
+                        <Pagination currentPage={currentPage}
+                                    totalPages={totalPages}
+                                    paginate={paginate}
+                        />
+                    }
+                </div>
+            </div>
+        </div>
+    );
+
+}
