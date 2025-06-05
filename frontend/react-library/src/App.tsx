3980fe95d4097819aadebe84ed7ff490b71e50a1
dishliysK9 <201221013@fdiba.tu-sofia.bg>
2025-06-05 20:58:09 +0300

    List of reviews

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -10,6 +10,7 @@ import {oktaConfig} from "./lib/oktaConfig";
 import {OktaAuth, toRelativeUrl} from "@okta/okta-auth-js";
 import {LoginCallback, Security} from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
+import {ReviewListPage} from "./layout/BookCheckoutPage/ReviewListPage/ReviewListPage";
 
 //react is SPA (single page application)
 
@@ -42,6 +43,9 @@ export const  App = () => {
                   </Route>
                   <Route path='/search' >
                       <SearchBooksPage/>
+                  </Route>
+                  <Route path='/reviewlist/:bookId' >
+                      <ReviewListPage/>
                   </Route>
                   <Route path='/checkout/:bookId'>
                       <BookCheckoutPage/>
