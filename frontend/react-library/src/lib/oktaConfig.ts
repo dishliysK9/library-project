c5499f57c71d55c0797172ea590e8e8ff7643dea
dishliysK9 <201221013@fdiba.tu-sofia.bg>
2025-09-26 00:04:56 +0300

    Redirect uri change

--- frontend/react-library/src/lib/oktaConfig.ts
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oaoldy0qqybF2e5H5d7',
     issuer: 'https://dev-55264191.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://library-service-react-961766531040.europe-west1.run.app/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
